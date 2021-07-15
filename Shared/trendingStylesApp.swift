0d7298bc69312c1507c429b6f166bdbf6bbda88c
Ridwan Sanusi <rosanusi@gmail.com>
2021-07-15 22:14:30 +0200

    Initial Commit

---
+++ Shared/trendingStylesApp.swift
@@ -0,0 +1,17 @@
+//
+//  trendingStylesApp.swift
+//  Shared
+//
+//  Created by Sanusi, Ridwan Oluwabusola on 15.07.21.
+//
+
+import SwiftUI
+
+@main
+struct trendingStylesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
